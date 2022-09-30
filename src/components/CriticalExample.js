c3c5321886d5999c7dde59c942d3a4a286a51599
Liam Merlyn <l.merlyn@hotmail.co.uk>
2022-09-30 16:54:38 +0100

    JS Fix

--- src/components/CriticalExample.js
+++ src/components/CriticalExample.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const getCritical = Array.from(document.styleSheets).filter((sheet) => { try { sheet.cssRules; return true; } catch (err) { console.log(`Due to CORS issues, this script can't access "${sheet.href}"`); return false; }}).map((sheet) => Array.from(sheet.cssRules).map((rule) => rule.type === rule.FONT_FACE_RULE ? rule.cssText : rule.type === rule.STYLE_RULE ? Array.from(document.querySelectorAll(rule.selectorText)).some((node) => node.getBoundingClientRect().top < (window.innerHeight / 3) && window.getComputedStyle(node).display !== 'none') ? rule.cssText : null : rule.type === rule.MEDIA_RULE ? `@media ${rule.conditionText} {${Array.from(rule.cssRules).map((mediaRule) => mediaRule.type === mediaRule.FONT_FACE_RULE ? mediaRule.cssText : mediaRule.type === mediaRule.STYLE_RULE ? Array.from(document.querySelectorAll(mediaRule.selectorText)).some((node) => node.getBoundingClientRect().top < window.innerHeight && window.getComputedStyle(node).display !== 'none') ? mediaRule.cssText : null : null).filter((mediaRule) => mediaRule != null).join(' ')}}` : null)).flat().filter((rule) => rule != null && !new RegExp(/^@media.*{}$/).test(rule)).join(' ');
 
@@ -12,5 +13,9 @@ function trimString(string) {
 }
 
 export default function CriticalExample() {
-  return <CodeBlock language="css">{trimString(getCritical)}</CodeBlock>
+  return (
+    <BrowserOnly>
+      <CodeBlock language="css">{trimString(getCritical)}</CodeBlock>
+    </BrowserOnly>
+  )
 }
